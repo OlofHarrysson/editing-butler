9ea95b2ca8a2fb8c0b3a92b62e3cfd1d76eeacd9
Olof Harrysson <harrysson.olof@gmail.com>
2020-02-01 17:16:09 +0100

    trying fetch

--- src/utils/update.py
+++ src/utils/update.py
@@ -7,21 +7,26 @@ import meta_utils
 
 
 def update_version():
+  ''' Saves changed files and resets repo to origin/master (i.e. updates) '''
   project_dir = meta_utils.get_project_root()
   repo = git.Repo(project_dir)
-  # Saves changed files and updates repo
-  print("WOOWOW")
   changed_files = [item.a_path for item in repo.index.diff(None)]
-  print(changed_files)
+
+  # Save changed files
   if changed_files:
     date_time = datetime.now().strftime("%Y%d%m-%H%M%S")
     save_dir = project_dir / 'saved_updatefiles' / f'date-{date_time}'
     save_dir.mkdir(parents=True)
     for f in changed_files:
       src_path = project_dir / f
-      assert src_path.exists()
       out_path = save_dir / f
+      out_path.parent.mkdir(parents=True, exist_ok=True)
+      assert src_path.exists()
       shutil.copy(src_path, out_path)
+
+    # Update repo
+    for remote in repo.remotes:
+      remote.fetch()
 
 
 if __name__ == '__main__':
